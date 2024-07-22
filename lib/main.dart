2823e25cd731508bfe9a9d30d61cc6115d80d25b
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-07-22 15:03:39 +0700

    chores: update configure compatibel with windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:qubah_app/app/common/app_colors.dart';
+import 'package:qubah_app/app/common/http_override.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+List<CameraDescription> cameras = [];
+void main() async {
+  HttpOverrides.global = MyHttpOverrides();
+  await GetStorage.init();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: AppColors.mainScheme,
     ),
   );
 }
