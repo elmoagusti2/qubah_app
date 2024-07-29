e5f4ffc143b04c1d275d059a6ed4ef4b91b9eded
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-07-29 11:50:20 +0700

    chores: init localizations and adding assets lottie, fonts and image

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class L10n {
+  static final all = [
+    const Locale('en'),
+    const Locale('id'),
+  ];
+}
+
+//useable
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// AppLocalizations.of(context)!.languange
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_messaging
+import flutter_image_compress_macos
+import flutter_local_notifications
 import geolocator_apple
 import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
